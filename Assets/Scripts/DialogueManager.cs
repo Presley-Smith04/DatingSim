64b0fb65fc04eada390df9838f8b848ad5c75164
Presley Smith <147758343+Presley-Smith04@users.noreply.github.com>
2025-05-16 19:18:23 -0400

    update

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,29 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using static UnityEngine.EventSystems.EventTrigger;
+
 
 public class DialogueManager : MonoBehaviour
 {
+    //dialogue and button data stuff
     public DialogueData currentDialogue;
     public TMP_Text dialogueText;
     public Button[] choiceButtons;
     public TMP_Text[] choiceTexts;
 
+
+    //set data to 0, wait for input to continue
     private int dialogueIndex = 0;
     private bool waitingForSpace = false;
     private int totalScore = 0;
+    private Coroutine typingCoroutine;
+
+
+    //girl sprites
+    public SpriteRenderer girlRenderer;
+    public Sprite smileSprite;
+    public Sprite neutralSprite;
+    public Sprite madSprite;
+
+
 
 
 
     void Start()
     {
-        // Get the current date number (defaults to 1)
+        //get date number
         int dateNumber = PlayerPrefs.GetInt("Date", 1);
 
-        // Build the dialogue asset name based on the date
+        //build dialogue based on date #
         string name = $"Date{dateNumber}Dialogue";
 
-        // Load the DialogueData asset from the Resources folder
+        //load dialogue
         currentDialogue = Resources.Load<DialogueData>(name);
 
         if (currentDialogue == null)
@@ -34,6 +50,7 @@ public class DialogueManager : MonoBehaviour
             return;
         }
 
+
         ShowDialogueLine();
     }
 
@@ -41,6 +58,7 @@ public class DialogueManager : MonoBehaviour
 
     void Update()
     {
+        //wait for input
         if (waitingForSpace && Input.GetKeyDown(KeyCode.Space))
         {
             waitingForSpace = false;
@@ -50,61 +68,146 @@ public class DialogueManager : MonoBehaviour
 
 
 
+
     void ShowDialogueLine()
     {
+        //show dialogue
         DialogueEntry entry = currentDialogue.dateDialogue[dialogueIndex];
-        dialogueText.text = entry.girlDialogue;
 
 
+        //typewriter effect
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+
+        typingCoroutine = StartCoroutine(TypeText(entry.girlDialogue));
+
+        //player choices
         for (int i = 0; i < 4; i++)
         {
             choiceTexts[i].text = entry.playerChoices[i];
         }
+
         EnableChoices(false);
+    }
+
+
+
+
+
+    //typewriter text style
+    IEnumerator TypeText(string line)
+    {
+        dialogueText.text = "";
+        waitingForSpace = false;
+
+
+        //typing sppeeed
+        foreach (char c in line.ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(0.03f);
+        }
+
         waitingForSpace = true;
     }
 
 
+
     void EnableChoices(bool enabled)
     {
+        //activate choices
         foreach (Button btn in choiceButtons)
             btn.gameObject.SetActive(enabled);
     }
 
 
 
+
     public void ChooseOption(int index)
     {
         DialogueEntry entry = currentDialogue.dateDialogue[dialogueIndex];
-        totalScore += entry.choicePoints[index];
-        dialogueText.text = entry.girlResponses[index];
+
+        int points = entry.choicePoints[index];
+        totalScore += points;
+
+        // Change her facial expression
+        switch (points)
+        {
+            case 0:
+                girlRenderer.sprite = madSprite;
+                break;
+            case 1:
+                girlRenderer.sprite = neutralSprite;
+                break;
+            case 2:
+                girlRenderer.sprite = smileSprite;
+                break;
+            default:
+                girlRenderer.sprite = neutralSprite;
+                break;
+        }
+
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        // Start coroutine to show response and wait for space
+        typingCoroutine = StartCoroutine(ShowResponseThenNext(entry.girlResponses[index]));
+
+        EnableChoices(false);
+    }
+
+
+
+    IEnumerator ShowResponseThenNext(string response)
+    {
+
+        //girl response
+        dialogueText.text = "";
+        waitingForSpace = false;
+
+
+        //typing speedd
+        foreach(char c in response.ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(0.03f);
+        }
+
+        waitingForSpace = true;
+
+
+
+        //wait for it to be pressed
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        waitingForSpace = false;
         dialogueIndex++;
 
+
+        //show dialogue if you havent
         if (dialogueIndex < currentDialogue.dateDialogue.Length)
         {
-            // Wait and show next line
-            Invoke("ShowDialogueLine", 1.5f);
-        }
-        else
+            ShowDialogueLine();
+        } else
         {
-            // Save score and increment date
+            //set score and increase date
             PlayerPrefs.SetInt("Score", totalScore);
             int currentDate = PlayerPrefs.GetInt("Date", 1);
             PlayerPrefs.SetInt("Date", currentDate + 1);
 
-            // Load next scene
-            if (currentDate < 3)
+
+            //next date
+            if(currentDate < 3)
             {
                 string nextScene = $"Date{currentDate + 1}";
                 UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);
-            }
-            else
+            } else
             {
-                UnityEngine.SceneManagement.SceneManager.LoadScene("EndScene");
+                //end 
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Ending");
+
             }
         }
-
-        EnableChoices(true);
     }
 
 
